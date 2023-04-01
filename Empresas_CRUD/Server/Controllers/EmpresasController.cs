95f8a831d9025c5d60385c718fc4f7f77c9c1e8c
freireguilherme <guilhermemagalhaesfreire22@gmail.com>
2023-04-01 11:34:40 -0300

    add: empresas crud controller

---
+++ Empresas_CRUD/Server/Controllers/EmpresasController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Empresas_CRUD.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmpresasController : ControllerBase
+    {
+        //alguns mocks
+        public static List<Segmentos> segmentos = new List<Segmentos> { };
+    }
+}
