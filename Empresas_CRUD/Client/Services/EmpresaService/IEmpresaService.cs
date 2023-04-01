f5a519b3ce57bc601ec0c5e10e761c9102140089
freireguilherme <guilhermemagalhaesfreire22@gmail.com>
2023-04-01 16:03:59 -0300

    add: interface IEmpresaService for class EmpresaService

--- Empresas_CRUD/Client/Services/EmpresaService/IEmpresaService.cs
+++ Empresas_CRUD/Client/Services/EmpresaService/IEmpresaService.cs
@@ -1,6 +1,14 @@
-﻿namespace Empresas_CRUD.Client.Services.EmpresaService
+﻿
+namespace Empresas_CRUD.Client.Services.EmpresaService
 {
     public interface IEmpresaService
     {
+        List<Empresas> Empresas { get; set; }
+        List<Segmentos> Segmentos { get; set; }
+
+        Task GetSegmentos();
+        Task GetEmpresas();
+
+        Task<Empresas> GetSingleEmpresa(int id);
     }
 }
