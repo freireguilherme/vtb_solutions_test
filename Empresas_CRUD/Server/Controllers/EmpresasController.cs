956bcd4323cbdd3c296aea7ee0ac81959a6d22bf
freireguilherme <guilhermemagalhaesfreire22@gmail.com>
2023-04-01 11:57:26 -0300

    add: controler mocks and get functions (list and a single based on id)

--- Empresas_CRUD/Server/Controllers/EmpresasController.cs
+++ Empresas_CRUD/Server/Controllers/EmpresasController.cs
@@ -8,6 +8,45 @@ namespace Empresas_CRUD.Server.Controllers
     public class EmpresasController : ControllerBase
     {
         //alguns mocks
-        public static List<Segmentos> segmentos = new List<Segmentos> { };
+        public static List<Segmentos> segmentos = new List<Segmentos> {
+            new Segmentos { Id = 1, Nome = "Indústria"},
+            new Segmentos { Id = 2, Nome = "Tecnologia"}
+        };
+
+        public static List<Empresas> empresas = new List<Empresas> {
+            new Empresas {
+                Id = 1,
+                Nome = "ACME",
+                Site = "https://acme.com",
+                Segmento = segmentos[0]
+            },
+            new Empresas {
+                Id = 2,
+                Nome = "Google",
+                Site = "https://google.com",
+                Segmento = segmentos[1]
+            }
+        };
+        //funções.
+        //Aqui queremos receber uma lista de todas as empresas de modo async
+        [HttpGet]
+        public async Task<ActionResult<List<Empresas>>> GetEmpresas()
+        {
+            return Ok(empresas);
+        }
+
+        //Agora queremos receber uma empresa com base no ID
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Empresas>> GetSingleEmpresa(int id)
+        {
+            var empresa = empresas.FirstOrDefault(e => e.Id == id);
+            if (empresa == null)
+            {
+                return NotFound("Desculpa, empresa não encontrada");
+            }
+            return Ok(empresas);
+        }
+
+
     }
 }
