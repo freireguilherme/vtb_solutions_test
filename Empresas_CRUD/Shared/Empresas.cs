4672ab82740cc4e9cfee84c6a2abc1b710ed0aa2
freireguilherme <guilhermemagalhaesfreire22@gmail.com>
2023-04-01 11:22:00 -0300

    add: class Empresas

---
+++ Empresas_CRUD/Shared/Empresas.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Empresas_CRUD.Shared
+{
+    public class Empresas
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; } = string.Empty;
+
+        public string Site { get; set; } = string.Empty;
+
+        public Segmentos? Segmento { get; set; }
+
+        public int SegmentoId { get; set; }
+    }
+}
