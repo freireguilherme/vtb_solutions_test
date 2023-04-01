390837aac4f393526efe23b792cf48d539931281
freireguilherme <guilhermemagalhaesfreire22@gmail.com>
2023-04-01 16:03:29 -0300

    add: Class EmpresaService

--- Empresas_CRUD/Client/Services/EmpresaService/EmpresaService.cs
+++ Empresas_CRUD/Client/Services/EmpresaService/EmpresaService.cs
@@ -1,7 +1,35 @@
-﻿namespace Empresas_CRUD.Client.Services.EmpresaService
+﻿using System.Net.Http.Json;
+
+namespace Empresas_CRUD.Client.Services.EmpresaService
 {
     //A classe Empresa Service é uma implementação da classe IEmpresaService (interface)
     public class EmpresaService : IEmpresaService
     {
+        private readonly HttpClient _http;
+
+        public EmpresaService(HttpClient http)
+        {
+            _http = http;
+        }
+        public List<Empresas> Empresas { get; set; } = new List<Empresas>();
+        public List<Segmentos> Segmentos { get; set; } = new List<Segmentos>();
+
+        public async Task GetEmpresas()
+        {
+            var resultado = await _http.GetFromJsonAsync<List<Empresas>>("api/empresas");
+
+            if (resultado != null)
+                Empresas = resultado;
+        }
+
+        public Task GetSegmentos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Empresas> GetSingleEmpresa(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
