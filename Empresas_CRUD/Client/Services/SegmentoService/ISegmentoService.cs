82c95d82706f48ee7834af75852b5e6ebd707a7b
freireguilherme <guilhermemagalhaesfreire22@gmail.com>
2023-04-09 12:52:39 -0300

    add: interface segmentoservice and class segmentoservice

---
+++ Empresas_CRUD/Client/Services/SegmentoService/ISegmentoService.cs
@@ -0,0 +1,18 @@
+﻿using Empresas_CRUD.Shared;
+
+namespace Empresas_CRUD.Client.Services.SegmentoService
+{
+    public interface ISegmentoService
+    {
+        
+        List<Segmentos> Segmentos { get; set; }
+
+        Task GetSegmentos();
+
+        Task<Segmentos> GetSingleSegmento(int id);
+
+        Task CreateSegmento(Segmentos segmento);
+        Task UpdateSegmento(Segmentos segmento);
+        Task DeleteSegmento(int id);
+    }
+}
---
+++ Empresas_CRUD/Client/Services/SegmentoService/SegmentoService.cs
@@ -0,0 +1,32 @@
+﻿namespace Empresas_CRUD.Client.Services.SegmentoService
+{
+    public class SegmentoService : ISegmentoService
+    {
+        public List<Segmentos> Segmentos { get; set; } = new List<Segmentos>();
+
+        public Task CreateSegmento(Segmentos segmento)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteSegmento(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task GetSegmentos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Segmentos> GetSingleSegmento(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateSegmento(Segmentos segmento)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
