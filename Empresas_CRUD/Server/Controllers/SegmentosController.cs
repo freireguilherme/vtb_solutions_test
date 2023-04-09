7193f756ee3739a6798d5681691033e7a3f4d90d
freireguilherme <guilhermemagalhaesfreire22@gmail.com>
2023-04-09 13:12:47 -0300

    add: na classe SegementoController, httpPost createSegmento method

--- Empresas_CRUD/Server/Controllers/SegmentosController.cs
+++ Empresas_CRUD/Server/Controllers/SegmentosController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Empresas_CRUD.Shared;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Empresas_CRUD.Server.Controllers
@@ -29,6 +30,22 @@ namespace Empresas_CRUD.Server.Controllers
             if (segmento == null)
                 return NotFound("Desculpa, segmento não encontrado");
             return Ok(segmento);
+        }
+
+        private async Task<List<Segmentos>> GetDbSegmentos()
+        {
+            return await _context.Segmentos.ToListAsync();
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult<List<Segmentos>>> CreateSemento( Segmentos segmento)
+        {
+
+            _context.Segmentos.Add(segmento);
+            await _context.SaveChangesAsync();
+
+            return Ok(await GetDbSegmentos());
         }
     }
 }
