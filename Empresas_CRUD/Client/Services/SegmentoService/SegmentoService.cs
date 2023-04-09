ec1bcc7790e17af9b506792ac4ef3fd24802550e
freireguilherme <guilhermemagalhaesfreire22@gmail.com>
2023-04-09 13:17:05 -0300

    add: implementação da classe segmentoService, construtor, createSegmento

--- Empresas_CRUD/Client/Services/SegmentoService/SegmentoService.cs
+++ Empresas_CRUD/Client/Services/SegmentoService/SegmentoService.cs
@@ -1,12 +1,30 @@
-﻿namespace Empresas_CRUD.Client.Services.SegmentoService
+﻿using Empresas_CRUD.Shared;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http.Json;
+
+namespace Empresas_CRUD.Client.Services.SegmentoService
 {
     public class SegmentoService : ISegmentoService
     {
+        private readonly HttpClient _http;
+        private readonly NavigationManager _navigationManager;
+        public SegmentoService(HttpClient http, NavigationManager navigationManager)
+        {
+            _http = http;
+            _navigationManager = navigationManager;
+        }
         public List<Segmentos> Segmentos { get; set; } = new List<Segmentos>();
 
-        public Task CreateSegmento(Segmentos segmento)
+        private async Task SetSegmento (HttpResponseMessage resultado)
         {
-            throw new NotImplementedException();
+            var resposta = await resultado.Content.ReadFromJsonAsync<List<Segmentos>>();
+            Segmentos = resposta;
+            _navigationManager.NavigateTo("listar-segmentos");
+        }
+        public async Task CreateSegmento(Segmentos segmento)
+        {
+            var resultado = await _http.PostAsJsonAsync("api/segmentos", segmento);
+            await SetSegmento(resultado);
         }
 
         public Task DeleteSegmento(int id)
