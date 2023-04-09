a6422054052555031f85be40641bc81fed664358
freireguilherme <guilhermemagalhaesfreire22@gmail.com>
2023-04-09 12:01:59 -0300

    add: segmentos controller. http get methods

---
+++ Empresas_CRUD/Server/Controllers/SegmentosController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Empresas_CRUD.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SegmentosController : ControllerBase
+    {
+        //construtor
+        private readonly DataContex _context;
+
+        public SegmentosController(DataContex context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Segmentos>>> GetSegmentos()
+        {
+            var segmentos = await _context.Segmentos.ToListAsync();
+            return Ok(segmentos);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Segmentos>> GetSingleSegmento (int id)
+        {
+            var segmento = await _context.Segmentos.FirstOrDefaultAsync(s => s.Id == id);
+            if (segmento == null)
+                return NotFound("Desculpa, segmento não encontrado");
+            return Ok(segmento);
+        }
+    }
+}
